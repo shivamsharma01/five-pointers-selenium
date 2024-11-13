b6304d80c6259cf36a8939879014f8b415b6cc1b
Shivam Sharma <shivamsharma.ade@gmail.com>
2024-11-13 22:33:50 +0530

    jagran news 2/x save to db

--- src/main/java/com/fivepointers/selenium/service/JagranNewsService.java
+++ src/main/java/com/fivepointers/selenium/service/JagranNewsService.java
@@ -1,4 +1,4 @@
-package com.fivepointers.selenium;
+package com.fivepointers.selenium.service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -11,10 +11,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
-import com.fivepointers.selenium.DriveSeleniumTask.Article;
+import com.fivepointers.selenium.model.Article;
+import com.fivepointers.selenium.model.NewsSection;
+import com.fivepointers.selenium.repository.NewsStoreRepository;
+import com.fivepointers.selenium.task.DriveSeleniumTask;
 
-public class Jagran extends News {
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Service
+public class JagranNewsService extends AbstractNewsService {
+
+	private static final Logger log = LoggerFactory.getLogger(JagranNewsService.class);
+
 	// SUN, 29 SEP 2024 01:34 PM (IST)
 	private static final DateTimeFormatter formatter = new DateTimeFormatterBuilder().parseCaseInsensitive()
 			.appendPattern("EEE, dd MMM yyyy hh:mm a (z)").toFormatter(Locale.ENGLISH);
@@ -25,33 +40,31 @@ public class Jagran extends News {
 			new NewsSection("International-PAKISTAN", "https://www.jagran.com/world/pakistan-news-hindi.html"),
 			new NewsSection("International-CHINA", "https://www.jagran.com/world/china-news-hindi.html"),
 			new NewsSection("International-MIDDLE-EAST", "https://www.jagran.com/world/middle-east-news-hindi.html"),
-			new NewsSection("Entertainment-Bollywood", "https://www.jagran.com/entertainment/bollywood-news-hindi.html"),
-			new NewsSection("Entertainment-Web-Series", "https://www.jagran.com/entertainment/web-series-review-news-hindi.html"),
+			new NewsSection("Entertainment-Bollywood",
+					"https://www.jagran.com/entertainment/bollywood-news-hindi.html"),
+			new NewsSection("Entertainment-Web-Series",
+					"https://www.jagran.com/entertainment/web-series-review-news-hindi.html"),
 			new NewsSection("Cricket-Bouncer", "https://www.jagran.com/cricket/bouncer-news-hindi.html"),
 			new NewsSection("Cricket-Headlines", "https://www.jagran.com/cricket/headlines-news-hindi.html"),
 			new NewsSection("Business", "https://www.jagran.com/business/biz-news-hindi.html")
-			
-			);
 
-	public Jagran(String root) {
-		super(root, "jagran");
+	);
+
+	public JagranNewsService(String newsChannelName, NewsStoreRepository newsStoreRepository) {
+		super(newsChannelName, newsStoreRepository);
 	}
 
-	@Override
-	public void run() {
+	public void scrapNews() {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		sections.forEach(section -> {
-			checkOrCreateFolderAndFiles(section.getTopic());
 			driver.get(section.getUrl());
 			List<WebElement> elements = driver.findElements(By.cssSelector(".stickySidebar ul li"));
 			List<Article> articles = elements.stream().map(element -> getUrl(element))
-					.filter(url -> url != null && !url.isBlank())
-//					.limit(2)
-					.map(url -> getFullContent(url))
+					.filter(url -> url != null && !url.isBlank()).map(url -> getFullContent(url))
 					.filter(article -> article != null && !article.getContent().isBlank())
-					.filter(article -> isNewArticle(article.getDate())
-							&& isUnreadArticle(article.getUrl(), section.getTopic()))
+					.filter(article -> article.getPublishDate().isAfter(DriveSeleniumTask.lastScraped)
+							|| isUnreadArticle(article.getUrl()))
 					.collect(Collectors.toList());
 			writeArticles(section.getTopic(), articles);
 		});
@@ -71,13 +84,14 @@ public class Jagran extends News {
 	private static Article getFullContent(String url) {
 		WebDriver driver = new ChromeDriver();
 		Article article = new Article();
+		article.setSaveDate(LocalDateTime.now());
 		article.setUrl(url);
 		driver.get(url);
 		try {
 			WebElement element = driver.findElement(By.className("main-col"));
 			wayOne(element, article);
 		} catch (org.openqa.selenium.NoSuchElementException ex) {
-			System.out.println("Not able to fetch news article with url " + article.getUrl());
+			log.error("Not able to fetch news article with url " + article.getUrl());
 			return null;
 		}
 		driver.close();
@@ -95,29 +109,29 @@ public class Jagran extends News {
 			textString = element.findElement(By.className("ArticleBody")).getText();
 			article.setContent(textString);
 		} catch (org.openqa.selenium.NoSuchElementException ex) {
-			System.out.println("Not able to fetch news article details: " + article.getUrl());
+			log.error("Not able to fetch news article details: " + article.getUrl());
 		}
 		try {
 			textString = element.findElement(By.tagName("h1")).getText();
 			article.setTitle(textString);
 		} catch (org.openqa.selenium.NoSuchElementException ex) {
-			System.out.println("Not able to fetch news article title: " + article.getUrl());
+			log.error("Not able to fetch news article title: " + article.getUrl());
 		}
 		try {
 			textString = element.findElement(By.cssSelector("span[class*='_date_']")).getText();
 			if (textString.contains(":")) {
-				article.setDate(LocalDateTime.parse(textString.split(":", 2)[1].trim(), formatter));
+				article.setPublishDate(LocalDateTime.parse(textString.split(":", 2)[1].trim(), formatter));
 			} else {
-				System.out.println("Not able to fetch news article date: " + article.getUrl());
+				log.error("Not able to fetch news article date: " + article.getUrl());
 			}
 		} catch (org.openqa.selenium.NoSuchElementException ex) {
-			System.out.println("Not able to fetch news article date: " + article.getUrl());
+			log.error("Not able to fetch news article date: " + article.getUrl());
 		}
 		try {
 			textString = element.findElement(By.cssSelector("p[class*='_shortdescription_']")).getText();
 			article.setSynopsys(textString);
 		} catch (org.openqa.selenium.NoSuchElementException ex) {
-			System.out.println("Not able to fetch news article synopsis: " + article.getUrl());
+			log.error("Not able to fetch news article synopsis: " + article.getUrl());
 		}
 	}
 
